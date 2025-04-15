cc67668fcf02ae52fa88342f4fb5d74454f45afd
nehamaryjohn <nehamary996@gmail.com>
2025-04-15 03:01:07 -0700

    LinkedList

---
+++ RemoveAll.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+public class RemoveAll{
+    public static void main(String[] args){
+        LinkedList<String>linkedList = new LinkedList<>();
+        linkedList.add("Apple");
+        linkedList.add("Banana");
+        linkedList.add("Cherry");
+        linkedList.add("Date");
+        
+        System.out.println("Intial LinkedList:"+linkedList);
+        linkedList.clear();
+         System.out.println("LinkedList after removing all elements:"+linkedList);
+    }
+}
